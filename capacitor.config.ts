1782449ca3d2e5e19d3a55ece82b7f9ff1493dda
SebaRLHM <sebastian.espinoza.r@mail.pucv.cl>
2025-11-02 20:48:55 -0300

    Eliminar archivos

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'vrassitant',
-  webDir: 'www'
-};
-
-export default config;
--- src/app/models/catchError.ts
+++
@@ -1,9 +0,0 @@
-async function catchError(promise:any) {
-  try {
-    let data = await promise;
-    return [data, null];
-  } catch (error) {
-    console.log
-    return [null, error];
-  }
-}
